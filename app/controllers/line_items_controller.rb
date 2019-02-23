00a7bf03a7619b5fe93390d0050d452b229857e4
wxie <wxie11@hawk.iit.edu>
2019-02-22 23:35:48 -0600

    create ajax-based cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,9 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to redirect_to store_index_url }
+        format.js
         format.json { render :show,
           status: :created, location: @line_item }
       else
